42674a84bd2a380ec54618ee2c288686d3ef4f04
KimKiHyun0206 <rlarlgus0206@naver.com>
2023-04-18 19:44:48 +0900

    update : Kruscal - Not Completed

---
+++ sample-code/src/MST_Kruscal/KruscalAlgorithm.java
@@ -0,0 +1,15 @@
+package MST_Kruscal;
+
+public class KruscalAlgorithm {
+    private final static int[][] graph = {
+            {0, 1, 2, 0, 3},
+            {1, 0, 3, 0, 2},
+            {2, 3, 0, 5, 1},
+            {0, 0, 5, 0, 0},
+            {3, 2, 1, 0, 0}
+    };
+
+    public static void main(String[] args) {
+
+    }
+}
