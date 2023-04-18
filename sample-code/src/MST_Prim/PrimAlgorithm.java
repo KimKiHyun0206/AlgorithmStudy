68f99b7170639e6e05ee777579a4c780859f9d4d
KimKiHyun0206 <rlarlgus0206@naver.com>
2023-04-18 20:16:14 +0900

    update : Prim

---
+++ sample-code/src/MST_Prim/PrimAlgorithm.java
@@ -0,0 +1,11 @@
+package MST_Prim;
+
+public class PrimAlgorithm {
+    public static void main(String[] args) {
+        //기준이 되는 정점 선택
+        //정점에서 이어지는 모든 간선 선택
+        //가장 짧은 간선이 이어지는 정점으로 이동
+        //사이클을 만들지 않는다는 가정 하에 모든 간선 추가
+        //모든 정점이 등록 완료될 때까지 반복
+    }
+}
