2e739836d08a22bd761a6be46a6629cc5a67645f
usr <rlarlgus0206@naver.com>
2023-05-21 15:58:58 +0900

    fix : 클래스 이름

--- sample-code/src/Chained_Matrix_Multiplications/model/Storage.java
+++ sample-code/src/Chained_Matrix_Multiplications/model/Storage.java
@@ -1,14 +1,18 @@
 package Chained_Matrix_Multiplications.model;
 
-public class CalculateResultStorage {
+public class Storage {
     private int[][] storage;
 
-    public CalculateResultStorage(int length) {
+    public Storage(int length) {
         this.storage = new int[length-1][length-1];
+
+        for(int i=0;i<length;i++){
+            storage[i][i] = 0;
+        }
     }
 
-    public int[][] getStorage() {
-        return storage;
+    public int getStorage(int x, int y) {
+        return this.storage[y][x];
     }
 
     public void setStorage(int x, int y, int value){
