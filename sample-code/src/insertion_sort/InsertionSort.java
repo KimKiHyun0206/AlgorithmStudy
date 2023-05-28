1dacce0ed3638b21af8a5de186a4cc54bcdd775b
KimKiHyun0206 <rlarlgus0206@naver.com>
2023-05-28 16:19:30 +0900

    update : insertion sort ready

---
+++ sample-code/src/insertion_sort/InsertionSort.java
@@ -0,0 +1,22 @@
+package insertion_sort;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class InsertionSort {
+    private final static int[] array = {7,6,5,4,9,8,2,1,3};
+    public static void main(String[] args) {
+
+    }
+}
+
+class Service {
+    public List<Integer> sort(int[] arr){
+
+        return null;
+    }
+}
+
+class View {
+
+}
