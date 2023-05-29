e6509495b1c7f69cced8f05f991d7ef7712aec23
KimKiHyun0206 <rlarlgus0206@naver.com>
2023-05-29 16:06:41 +0900

    create : Insertion Sort

--- sample-code/src/insertion_sort/InsertionSort.java
+++ sample-code/src/insertion_sort/InsertionSort.java
@@ -1,22 +1,40 @@
 package insertion_sort;
 
+import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
 
 public class InsertionSort {
-    private final static int[] array = {7,6,5,4,9,8,2,1,3};
-    public static void main(String[] args) {
+    private final static int[] array = {7, 6, 5, 4, 9, 8, 2, 1, 3};
 
+    public static void main(String[] args) {
+        Service service = new Service();
+        new View().showResult(
+                service.sort(array)
+        );
     }
 }
 
 class Service {
-    public List<Integer> sort(int[] arr){
+    public List<Integer> sort(int[] arr) {
 
-        return null;
+        List<Integer> resultList = new LinkedList<>();
+
+        resultList.add(arr[0]);
+
+        for (int i = 1; i < arr.length; i++) {
+            resultList.add(arr[i]);
+            resultList.sort(Comparator.naturalOrder());
+        }
+
+        return resultList;
     }
 }
 
 class View {
-
+    public void showResult(List<Integer> arr) {
+        for (int value:arr) {
+            System.out.print(value+" ");
+        }
+    }
 }
