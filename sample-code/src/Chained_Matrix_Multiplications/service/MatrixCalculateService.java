c154ecf2bc69bae4e1715109e4d0e5ad77f6bdc7
KimKiHyun0206 <rlarlgus0206@naver.com>
2023-06-02 22:18:28 +0900

    delete : method

--- sample-code/src/Chained_Matrix_Multiplications/service/MatrixCalculateService.java
+++ sample-code/src/Chained_Matrix_Multiplications/service/MatrixCalculateService.java
@@ -3,13 +3,4 @@ package Chained_Matrix_Multiplications.service;
 import Chained_Matrix_Multiplications.model.Storage;
 
 public class MatrixCalculateService {
-    public int calculate(int x, int y, int lastX, int lastY, Storage storage) {
-        int minValue = 0;
-
-        for (int i = x; i < lastX; i++) {
-            int calculateResult = storage.getStorage(x,y) +
-        }
-
-        return minValue;
-    }
 }
