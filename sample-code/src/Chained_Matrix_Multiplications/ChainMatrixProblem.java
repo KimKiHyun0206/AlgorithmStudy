62642b3288e9df7a153bdce950496029a10b6fd8
KimKiHyun0206 <rlarlgus0206@naver.com>
2023-06-04 11:45:23 +0900

    update : calculate log

--- sample-code/src/Chained_Matrix_Multiplications/ChainMatrixProblem.java
+++ sample-code/src/Chained_Matrix_Multiplications/ChainMatrixProblem.java
@@ -17,22 +17,28 @@ public class ChainMatrixProblem {
         }
 
         long[][] dp = new long[numberOfMatrix + 1][numberOfMatrix + 1];          //계산 내용 기록용 배열
+        //op는 계산할 배열의 위치를 맞추기 위함
+        //대각선이잖아 맞춰야
         for (int op = 1; op < numberOfMatrix; op++) {                            //행렬의 갯수만큼 연산
+            System.out.println("[ Try ] " + op);
             for (int y = 1; y <= numberOfMatrix - op; y++) {
 
                 int x = y + op;
-                dp[y][x] = Long.MAX_VALUE;
+                dp[y][x] = Long.MAX_VALUE;    //계산하기 전에 INF 로 초기화한다.
 
                 for (int k = y; k < x; k++) {
+                    long compareValue = dp[y][k] + dp[k + 1][x] + (long) matrixInfo[y - 1] * matrixInfo[k] * matrixInfo[x];
                     dp[y][x] = Math.min(
-                            dp[y][k] + dp[k + 1][x] + (long) matrixInfo[y - 1] * matrixInfo[k] * matrixInfo[x],
+                            compareValue,
                             dp[y][x]
                     );
+                    System.out.println("[ Compare ] x :" + x + ", y :" + y + ", compareValue : " + compareValue);
                 }
             }
+            System.out.println();
         }
 
-        for (int i = 1; i < dp.length ; i++) {
+        for (int i = 1; i < dp.length; i++) {
             System.out.println(Arrays.toString(dp[i]));
         }
 
