e483125370f207eed135b1ede942596f85208fff
KimKiHyun0206 <rlarlgus0206@naver.com>
2023-06-06 20:22:52 +0900

    update

--- sample-code/src/문자열편집/Levenshtein.java
+++ sample-code/src/문자열편집/Levenshtein.java
@@ -23,11 +23,11 @@ public class Levenshtein {
 
         int[][] d = new int[m + 1][n + 1];
 
-        for (int i = 1; i < m; i++) {
+        for (int i = 0; i < m; i++) {
             d[i][0] = i;
         }
 
-        for (int j = 1; j < n; j++) {
+        for (int j = 0; j < n; j++) {
             d[0][j] = j;
         }
 
